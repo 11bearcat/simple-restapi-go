b5d3eda397397a2ab21119b7e6fefab32f2bca4b
Bereket Kaziyev <3331405-bearcat11@users.noreply.gitlab.com>
2021-11-10 03:38:19 +0600

    log request middleware

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"github.com/google/uuid"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
+	"github.com/sirupsen/logrus"
 	"go-rest-api/internal/app/model"
 	"go-rest-api/internal/app/store"
 	"net/http"
+	"time"
 )
 
 var (
@@ -19,12 +23,14 @@ var (
 const (
 	sessionName = "test"
 	ctxKeyUser ctxKey = iota
+	ctxKeyRequestID
 )
 
 type ctxKey int8
 
 type server struct {
 	router 			*mux.Router
+	logger 			*logrus.Logger
 	store 			store.Store
 	sessionStore 	sessions.Store
 }
@@ -32,6 +38,7 @@ type server struct {
 func newServer(store store.Store, sessionsStore sessions.Store) *server {
 	s := &server {
 		router: 		mux.NewRouter(),
+		logger: 		logrus.New(),
 		store: 			store,
 		sessionStore: 	sessionsStore,
 	}
@@ -46,6 +53,9 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) configureRouter() {
+	s.router.Use(s.setRequestID)
+	s.router.Use(s.logRequest)
+	s.router.Use(handlers.CORS(handlers.AllowedOrigins([]string{"*"})))
 	s.router.HandleFunc("/users", s.handleUsersCreate()).Methods("POST")
 	s.router.HandleFunc("/sessions", s.handleSessionsCreate()).Methods("POST")
 
@@ -53,6 +63,34 @@ func (s *server) configureRouter() {
 	private := s.router.PathPrefix("/private").Subrouter()
 	private.Use(s.authenticateUser)
 	private.HandleFunc("/whoami", s.handleWhoami()).Methods("GET")
+}
+
+func (s *server) setRequestID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.New().String()
+		w.Header().Set("X-Request-ID", id)
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), ctxKeyRequestID, id)))
+	})
+}
+
+func (s *server) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := s.logger.WithFields(logrus.Fields{
+			"remote_addr": r.RemoteAddr,
+			"request_id": r.Context().Value(ctxKeyRequestID),
+		})
+		logger.Infof("started %s %s", r.Method, r.RequestURI)
+
+		start := time.Now()
+		rw := &responseWriter{w, http.StatusOK}
+		next.ServeHTTP(rw, r)
+		logger.Infof(
+			"completed with %d %s in %v",
+			rw.code,
+			http.StatusText(rw.code),
+			time.Now().Sub(start),
+		)
+	})
 }
 
 func (s *server) authenticateUser(next http.Handler) http.Handler {
